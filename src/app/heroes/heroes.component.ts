966636b721886e72df9caee304ed83da6b2c3955
DMChang-5R <r03b22043@ntu.edu.tw>
2017-11-07 17:04:13 +0800

    tutorial 7: routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  
+ 
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
@@ -24,7 +19,6 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
  
-
   ngOnInit() {
     this.getHeroes();
   }
