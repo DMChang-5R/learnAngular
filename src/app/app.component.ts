04e0985dee0f7e2b24156168f5f560785009e392
DMChang-5R <r03b22043@ntu.edu.tw>
2017-11-07 09:51:53 +0800

    tutorial 4: displaying a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,7 @@ const HEROES: Hero[] = [
   selector: 'app-root',
   template: `
     <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
+    <app-heroes></app-heroes>
     `,
   styleUrls: ['./app.component.css']
 })
